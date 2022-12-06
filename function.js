08738296126771a5254fa53e473f50db0daacf5b
Bandage2 <singhnandini9358@gmail.com>
2022-12-06 17:50:05 +0530

    repository

---
+++ function.js
@@ -0,0 +1,525 @@
+
+     // banner hide function
+
+        $(document).ready(function(){
+            $('#banner_hide').click(function(){
+              $('#banner_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.col-ting').removeClass('hide');
+              $('.col-ting').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#banner_show').click(function(){
+              $('#banner_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.col-ting').removeClass('show');
+              $('.col-ting').addClass('hide');
+            });
+        });
+
+
+
+// recipes hide function
+
+
+          $(document).ready(function(){
+            $('#recipes_hide').click(function(){
+              $('#recipes_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.recipe_name').removeClass('hide');
+              $('.recipe_name').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#recipes_show').click(function(){
+              $('#recipes_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.recipe_name').removeClass('show');
+              $('.recipe_name').addClass('hide');
+            });
+        });
+
+
+         //label_printer function
+
+
+         $(document).ready(function(){
+            $('#label_printer_hide').click(function(){
+              $('#label_printer_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.label_printer').removeClass('hide');
+              $('.label_printer').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#label_printer_show').click(function(){
+              $('#label_printer_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.label_printer').removeClass('show');
+              $('.label_printer').addClass('hide');
+            });
+        });
+
+         // right_part ingredient_list
+         
+
+         $(document).ready(function(){
+            $('#Enthält-Allergene_hide').click(function(){
+              $('#Enthält-Allergene_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Enthält-Allergene').removeClass('hide');
+              $('.Enthält-Allergene').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Enthält-Allergene_show').click(function(){
+              $('#Enthält-Allergene_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Enthält-Allergene').removeClass('show');
+              $('.Enthält-Allergene').addClass('hide');
+            });
+        });
+    
+
+       // Zutaten hide show function
+
+         $(document).ready(function(){
+            $('#Zutaten_hide').click(function(){
+              $('#Zutaten_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Zutaten').removeClass('hide');
+              $('.Zutaten').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Zutaten_show').click(function(){
+              $('#Zutaten_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Zutaten').removeClass('show');
+              $('.Zutaten').addClass('hide');
+            });
+        });
+
+         //hinweise_steps hide function
+
+
+         $(document).ready(function(){
+            $('#Hinweise_hide').click(function(){
+              $('#Hinweise_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Hinweise').removeClass('hide');
+              $('.Hinweise').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Hinweise_show').click(function(){
+              $('#Hinweise_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Hinweise').removeClass('show');
+              $('.Hinweise').addClass('hide');
+            });
+        });
+
+        // Deklarationshinweis hide function
+
+
+
+         $(document).ready(function(){
+            $('#Deklarationshinweis_hide').click(function(){
+              $('#Deklarationshinweis_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Deklarationshinweis').removeClass('hide');
+              $('.Deklarationshinweis').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Deklarationshinweis_show').click(function(){
+              $('#Deklarationshinweis_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Deklarationshinweis').removeClass('show');
+              $('.Deklarationshinweis').addClass('hide');
+            });
+        });
+
+         // table hide function
+
+         $(document).ready(function(){
+            $('#artikel-details_hide').click(function(){
+              $('#artikel-details_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.artikel-details').removeClass('hide');
+              $('.artikel-details').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#artikel-details_show').click(function(){
+              $('#artikel-details_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.artikel-details').removeClass('show');
+              $('.artikel-details').addClass('hide');
+            });
+        });
+
+         // address hide function
+
+
+         $(document).ready(function(){
+            $('#middle_footer_hide').click(function(){
+              $('#middle_footer_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.middle_footer').removeClass('hide');
+              $('.middle_footer').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#middle_footer_show').click(function(){
+              $('#middle_footer_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.middle_footer').removeClass('show');
+              $('.middle_footer').addClass('hide');
+            });
+        });
+
+
+        // Firma hide function
+
+
+         $(document).ready(function(){
+            $('#Firma_hide').click(function(){
+              $('#Firma_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Firma').removeClass('hide');
+              $('.Firma').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Firma_show').click(function(){
+              $('#Firma_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Firma').removeClass('show');
+              $('.Firma').addClass('hide');
+            });
+        });
+
+         //Mühlenkamp hide function
+
+         $(document).ready(function(){
+            $('#Mühlenkamp_hide').click(function(){
+              $('#Mühlenkamp_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Mühlenkamp').removeClass('hide');
+              $('.Mühlenkamp').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Mühlenkamp_show').click(function(){
+              $('#Mühlenkamp_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Mühlenkamp').removeClass('show');
+              $('.Mühlenkamp').addClass('hide');
+            });
+        });
+
+         // pincode hide function
+
+        $(document).ready(function(){
+            $('#pincode_hide').click(function(){
+              $('#pincode_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.pincode').removeClass('hide');
+              $('.pincode').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#pincode_show').click(function(){
+              $('#pincode_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.pincode').removeClass('show');
+              $('.pincode').addClass('hide');
+            });
+        });
+
+         // Stadt hide function
+
+
+         $(document).ready(function(){
+            $('#Stadt_hide').click(function(){
+              $('#Stadt_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Stadt').removeClass('hide');
+              $('.Stadt').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Stadt_show').click(function(){
+              $('#Stadt_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Stadt').removeClass('show');
+              $('.Stadt').addClass('hide');
+            });
+        });
+
+         // Land hide function
+
+
+         $(document).ready(function(){
+            $('#Land_hide').click(function(){
+              $('#Land_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Land').removeClass('hide');
+              $('.Land').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Land_show').click(function(){
+              $('#Land_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Land').removeClass('show');
+              $('.Land').addClass('hide');
+            });
+        });
+
+         // email hide function
+
+         $(document).ready(function(){
+            $('#email_hide').click(function(){
+              $('#email_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.email').removeClass('hide');
+              $('.email').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#email_show').click(function(){
+              $('#email_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.email').removeClass('show');
+              $('.email').addClass('hide');
+            });
+        });
+
+       
+         // phone number hide function
+
+         $(document).ready(function(){
+            $('#phone_hide').click(function(){
+              $('#phone_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.phone').removeClass('hide');
+              $('.phone').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#phone_show').click(function(){
+              $('#phone_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.phone').removeClass('show');
+              $('.phone').addClass('hide');
+            });
+        });
+
+         // Artikelnummer hide function
+
+         $(document).ready(function(){
+            $('#Artikelnummer_hide').click(function(){
+              $('#Artikelnummer_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Artikelnummer').removeClass('hide');
+              $('.Artikelnummer').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#Artikelnummer_show').click(function(){
+              $('#Artikelnummer_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.Artikelnummer').removeClass('show');
+              $('.Artikelnummer').addClass('hide');
+            });
+        });
+
+
+      // gitn-stuck hide function
+
+         $(document).ready(function(){
+            $('#gitn-stuck_hide').click(function(){
+              $('#gitn-stuck_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-stuck').removeClass('hide');
+              $('.gitn-stuck').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#gitn-stuck_show').click(function(){
+              $('#gitn-stuck_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-stuck').removeClass('show');
+              $('.gitn-stuck').addClass('hide');
+            });
+        });
+
+         //gitn-verpackung hide function
+
+         $(document).ready(function(){
+            $('#gitn-verpackung_hide').click(function(){
+              $('#gitn-verpackung_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-verpackung').removeClass('hide');
+              $('.gitn-verpackung').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#gitn-verpackung_show').click(function(){
+              $('#gitn-verpackung_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-verpackung').removeClass('show');
+              $('.gitn-verpackung').addClass('hide');
+            });
+        });
+
+
+         //gitn-gewicht hide function
+
+         $(document).ready(function(){
+            $('#gitn-gewicht_hide').click(function(){
+              $('#gitn-gewicht_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-gewicht').removeClass('hide');
+              $('.gitn-gewicht').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#gitn-gewicht_show').click(function(){
+              $('#gitn-gewicht_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.gitn-gewicht').removeClass('show');
+              $('.gitn-gewicht').addClass('hide');
+            });
+        });
+
+
+         // marke hide function
+
+         $(document).ready(function(){
+            $('#marke_hide').click(function(){
+              $('#marke_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.marke').removeClass('hide');
+              $('.marke').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#marke_show').click(function(){
+              $('#marke_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.marke').removeClass('show');
+              $('.marke').addClass('hide');
+            });
+        });
+
+          // qualitat hide function
+
+
+         $(document).ready(function(){
+            $('#qualitat_hide').click(function(){
+              $('#qualitat_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.qualitat').removeClass('hide');
+              $('.qualitat').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#qualitat_show').click(function(){
+              $('#qualitat_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.qualitat').removeClass('show');
+              $('.qualitat').addClass('hide');
+            });
+        });
+
+
+           // okokontrollstelle hide function
+
+         $(document).ready(function(){
+            $('#okokontrollstelle_hide').click(function(){
+              $('#okokontrollstelle_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.okokontrollstelle').removeClass('hide');
+              $('.okokontrollstelle').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#okokontrollstelle_show').click(function(){
+              $('#okokontrollstelle_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.okokontrollstelle').removeClass('show');
+              $('.okokontrollstelle').addClass('hide');
+            });
+        });
+
+
+         // herkunft hide function
+
+         $(document).ready(function(){
+            $('#herkunft_hide').click(function(){
+              $('#herkunft_show').removeClass('hide');
+              $(this).addClass('hide');
+              $('.herkunft').removeClass('hide');
+              $('.herkunft').addClass('show');
+            });
+        });
+         $(document).ready(function(){
+            $('#herkunft_show').click(function(){
+              $('#herkunft_hide').removeClass('hide');
+              $(this).addClass('hide');
+              $('.herkunft').removeClass('show');
+              $('.herkunft').addClass('hide');
+            });
+        });
+
+
+      
+           
+          //  Artikel hide function
+
+          $(document).ready(function(){
+          $('#list1_show').click(function(){
+            $('#list1_hide').removeClass('hide1');
+            $(this).addClass('hide1');
+            $('#Artikel_Details').removeClass('hide1');
+            $('#Artikel_Details').addClass('show1');
+          });
+         });
+
+         $(document).ready(function(){
+          $('#list1_hide').click(function(){
+            $('#list1_show').removeClass('hide1');
+            $(this).addClass('hide1');
+            $('#Artikel_Details').removeClass('show1');
+            $('#Artikel_Details').addClass('hide1');
+          });
+         });
+
+
+
+
+         //  contact_detail hide function
+
+          $(document).ready(function(){
+          $('#list_show').click(function(){
+            $('#list_hide').removeClass('hide1');
+            $(this).addClass('hide1');
+            $('#contact_detail').removeClass('hide1');
+            $('#contact_detail').addClass('show1');
+          });
+         });
+
+         $(document).ready(function(){
+          $('#list_hide').click(function(){
+            $('#list_show').removeClass('hide1');
+            $(this).addClass('hide1');
+            $('#contact_detail').removeClass('show1');
+            $('#contact_detail').addClass('hide1');
+          });
+         });
+
